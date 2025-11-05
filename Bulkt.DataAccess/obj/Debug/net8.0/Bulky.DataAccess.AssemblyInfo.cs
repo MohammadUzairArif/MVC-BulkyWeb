c2dbacbdde576c10bab572695e730c6f872d4c4c
Muhammad Uzair Arif <uzairarif28@gmail.com>
2025-11-06 03:28:26 +0500

    implement Repository pattern

--- Bulkt.DataAccess/obj/Debug/net8.0/Bulky.DataAccess.AssemblyInfo.cs
+++ Bulkt.DataAccess/obj/Debug/net8.0/Bulky.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a06b000646c5f847a1226997c7c330917358746")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59d69f3685e0839c86871514d99d742aeaffbdd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
