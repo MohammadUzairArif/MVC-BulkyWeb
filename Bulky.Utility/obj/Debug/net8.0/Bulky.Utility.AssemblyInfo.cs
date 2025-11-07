eb975a8fd8712146cee99f3a4d0abd74571f8e8a
Muhammad Uzair Arif <uzairarif28@gmail.com>
2025-11-08 02:44:09 +0500

    Add Product Table in DB

--- Bulky.Utility/obj/Debug/net8.0/Bulky.Utility.AssemblyInfo.cs
+++ Bulky.Utility/obj/Debug/net8.0/Bulky.Utility.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bulky.Utility")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48b9bd8ed3c03b1302eb1d4e8e8387718f21feef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab73dd95038a4d43e87c2e9037b9467573ff2abd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bulky.Utility")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bulky.Utility")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
