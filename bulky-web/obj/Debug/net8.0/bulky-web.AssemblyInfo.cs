590bab65b89eaef9eac9bd67953a1f92ed62e4b3
Muhammad Uzair Arif <uzairarif28@gmail.com>
2026-01-13 21:46:02 +0500

    Order summary ui

--- bulky-web/obj/Debug/net8.0/bulky-web.AssemblyInfo.cs
+++ bulky-web/obj/Debug/net8.0/bulky-web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bulky-web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+624a298f331bd816abadfff51c26e9d02f29376e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+227a14cdb7cbca37b2c8292fa2d399da3043fa32")]
 [assembly: System.Reflection.AssemblyProductAttribute("bulky-web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bulky-web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
